1a9dfcdff898b463c5bf3da339302f250a040b6b
devNdiplomacy <devndiplomacy@gmail.com>
2025-04-20 15:32:59 +0530

    Added Db feature

---
+++ composeApp/src/commonMain/kotlin/com/devndiplomacy/spendwise/db/SpendWiseDb.kt
@@ -0,0 +1,20 @@
+package com.devndiplomacy.spendwise.db
+
+import androidx.room.ConstructedBy
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.*
+import com.devndiplomacy.spendwise.db.dao.ExpenseDao
+import com.devndiplomacy.spendwise.db.models.ExpenseEntity
+
+@Database(entities = [ExpenseEntity::class], version = 1)
+@ConstructedBy(SpendWiseDatabaseConstructor::class)
+abstract class SpendWiseDb: RoomDatabase() {
+
+    abstract fun getExpenseDao(): ExpenseDao
+}
+
+@Suppress("NO_ACTUAL_FOR_EXPECT")
+expect object SpendWiseDatabaseConstructor : RoomDatabaseConstructor<SpendWiseDb> {
+    override fun initialize(): SpendWiseDb
+}
